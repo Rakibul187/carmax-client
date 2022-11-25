2ca3dab9e2a2203cda179a4cab876fc79b3cde52
Rakibul Islam <rakibul241221@gmail.com>
2022-11-25 22:35:50 +0600

    category wise data load & show

--- src/pages/Products/CategoriesProducts/CategoriesProducts.js
+++ src/pages/Products/CategoriesProducts/CategoriesProducts.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import Products from './Products';
 
 const CategoriesProducts = () => {
     const products = useLoaderData()
     return (
         <div className=' max-w-[1200px] mx-auto mt-10'>
-            <p>Products :  {products.length}</p>
+            <p className='text-3xl mb-5 text-primary font-semibold'>Here is all  {products[0].Category} Collection</p>
+            <div>
+                {
+                    products?.map(product =>
+                        <Products
+                            key={product._id}
+                            product={product}
+                        ></Products>)
+                }
+            </div>
         </div>
     );
 };
---
+++ src/pages/Products/CategoriesProducts/Products.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+const Products = ({ product }) => {
+    console.log(product)
+    const { img, description, mobile, postTime, productCondition, purchasePrice, resellPrice, sellerNmae, yearOfPurchase, yearOfUse } = product
+    return (
+        <div>
+            <div className="card lg:card-side bg-base-100 shadow-xl mb-10">
+                <figure className='w-[40vw] p-8 sm:mx-auto'><img src={img} alt="Album" /></figure>
+                <div className="card-body">
+                    <h2 className="card-title">  {product.productName}</h2>
+                    <p className='text-slate-500'>{description}</p>
+                    <div className='grid lg:grid-cols-3 md:grid-cols-2'>
+                        <p className='text-secondary'><span >Purchase Year: </span> {yearOfPurchase}</p>
+                        <p className='text-secondary'><span >Product Quality: </span> {productCondition}</p>
+                        <p className='text-secondary'><span >Purchase Price: </span>${purchasePrice} </p>
+                        <p className='text-secondary'><span >Resale Price: </span> ${resellPrice}</p>
+                        <p className='text-secondary'><span >Seller: </span> {sellerNmae} </p>
+                        <p className='text-secondary'><span >Contact: </span> {mobile}</p>
+                        <p className='text-secondary'><span >Post Time: </span> {postTime}</p>
+                        <p className='text-secondary'> <span >Used: </span> {yearOfUse}</p>
+                        <div className="card-actions justify-end">
+                        </div>
+                    </div>
+                    <div className='flex justify-end'>
+                        <button className="btn btn-primary">Booking Now</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
+};
+
+export default Products;
