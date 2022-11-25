143a0d4cb8bcd54f2943de7016378e29aab97b28
Rakibul Islam <rakibul241221@gmail.com>
2022-11-26 00:49:36 +0600

    design update

--- src/pages/Products/Categories/Categories.js
+++ src/pages/Products/Categories/Categories.js
@@ -12,14 +12,22 @@ const Categories = () => {
     console.log(categories)
     return (
         <div className='mt-20'>
-            <p>Our All Products Categories {categories.length}</p>
-            {
-                categories?.map(category =>
-                    <p key={category._id}>
-                        <Link to={`/category/${category?.Category}`}>{category?.Category}</Link>
-                    </p>)
-            }
-        </div>
+            <p className='text-3xl text-center'>We Sell </p>
+            <p><span className='border border-t-2'></span></p>
+            <div className='grid lg:grid-cols-3 md:grid-cols-2 sm:grid-cols-1 gap-5 sm:mx-auto'>
+                {
+                    categories?.map(category =>
+                        <div key={category._id} className="card w-96 bg-base-100 shadow-xl">
+                            <div className="card-body">
+                                <div className="card-actions">
+                                    <Link to={`/category/${category?.Category}`}><button className="btn btn-primary">{category.Category}</button></Link>
+                                </div>
+                            </div>
+                        </div>
+                    )
+                }
+            </div >
+        </div >
     );
 };
 
